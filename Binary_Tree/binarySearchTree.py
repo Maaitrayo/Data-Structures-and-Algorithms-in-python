d8b4277c71d5a073f615107ddbe09bac08d95670
Maaitrayo Das <dasmaaitrayo@gmail.com>
2023-05-08 15:30:06 +0530

    binary search tree

---
+++ Binary_Tree/binarySearchTree.py
@@ -0,0 +1,78 @@
+class BinarySearchTreeNode:
+    def __init__(self, data) -> None:
+        self.data = data
+        self.left = None
+        self.right = None
+    
+    def add_child(self,data):
+        if data == self.data:
+            return
+        
+        if data < self.data:
+            # add data in left subtree
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            # add data in right subtree
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
+    
+    def in_order_traversal(self):
+        elements = []
+
+        # visit left tree
+        if self.left:
+            elements += self.left.in_order_traversal()
+        
+        # visit base node
+        elements.append(self.data)
+
+        # visit right tree
+        if self.right:
+            elements += self.right.in_order_traversal()
+
+        return elements
+    
+    def search(self, val):
+        if self.data == val:
+            return True
+        
+        if val < self.data:
+            # val might be in left subtree
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False
+            
+        if val > self.data:
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
+
+
+
+def build_tree(elements):
+    root = BinarySearchTreeNode(elements[0])
+
+    for i in range(1,len(elements)):
+        root.add_child(elements[i])
+    
+    return root
+
+if __name__ == '__main__':
+    numbers = [17,4,1,20,9,23,18,34, 18,4]
+    numbers_tree = build_tree(numbers)
+    # print(numbers_tree.in_order_traversal())
+    print(numbers_tree.search(21))
+
+    countries = ["India","Pakistan","Germany", "USA","China","India","UK","USA"]
+    country_tree = build_tree(countries)
+
+    print("UK is in the list? ", country_tree.search("UK"))
+    print("Sweden is in the list? ", country_tree.search("Sweden"))
+
